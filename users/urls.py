459c5a959673c7490e7f1a46a7a238fd41a5444d
黄原寅 <3443718488@qq.com>
2023-11-19 18:42:23 +0800

    用户注册接口的实现

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import LoginView
+from users.views import LoginView,RegisterView
 
 urlpatterns = [
-    path('login/', LoginView.as_view())
+    path('login/', LoginView.as_view()),
+    path('register/', RegisterView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,9 +1,53 @@
+import re
+
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.views import APIView
+
+from users.models import User
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        """用户注册"""
+        # 1. 接收用户参数
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        password_confirmation = request.data.get('password_confirmation')
+
+        # 2. 参数校验
+        # 2.1 校验参数是否为空
+        if not all([username, email, password, password_confirmation]):
+            return Response({'error': "所有参数不能为空"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # 2.2 校验用户名是否为空
+        if User.objects.filter(username=username).exists():
+            return Response({'error': "用户名已存在"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # 2.3 校验两次密码是否一致
+        if password != password_confirmation:
+            return Response({'error': "两次密码不一致"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # 2.4 校验密码长度
+        if not (6 <= len(password) <= 18):
+            return Response({'error': "密码长度需要在6到18位之间"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # 2.5 校验邮箱是否存在
+        if User.objects.filter(email=email).exists():
+            return Response({'error': "该邮箱已被其他用户使用"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # 2.6 校验邮箱格式是否正确
+        if not re.match(r'^[a-z0-9][\w.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return Response({'error': "邮箱格式有误！"}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+        # 3. 创建用户
+        obj = User.objects.create_user(username=username, email=email, password=password)
+        res = {
+            'username': username,
+            'id': obj.id,
+            'email': obj.email
+        }
+        return Response(res, status=status.HTTP_201_CREATED)
 
 
 # Create your views here.
